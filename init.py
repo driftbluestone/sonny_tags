12673c026fd624355d7d463574a1c6551be534d6
driftbluestone <driftbluestone@gmail.com>
2026-05-25 15:23:50 -0700

    Update init.py

--- init.py
+++ init.py
@@ -3,7 +3,9 @@ from api import config, permission, users
 from pathlib import Path
 DATA_DIR = Path(__file__).resolve().parent.parent.parent / "data/extensions/sonny_tags/tags"
 
-permission.create(["view", "create", "admin"], [None, None, "administrator"])
+permission.create("view", "View Tags", None, True, True, True)
+permission.create("create", "Create Tags", None, True, True, True)
+permission.create("admin", "Tag Admin", "administrator", True, False, True)
 users.new_data_field("tags", list)
 config.create_field("limit_creation_to_admins", False)
 if not os.path.exists(DATA_DIR):
