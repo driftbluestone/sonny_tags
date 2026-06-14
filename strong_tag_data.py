2b3570c02e8e03e3e6ecfa7374822985d2082305
driftbluestone <driftbluestone@gmail.com>
2026-06-14 01:59:05 -0700

    last one hopefully

--- strong_tag_data.py
+++ strong_tag_data.py
@@ -1,5 +1,5 @@
 from utils import callable_module as CallableModule, jsonIO
 from pathlib import Path
 DIR = Path(__file__).resolve().parent
-SPECIAL_TAGS, DISPLAYED_SPECIAL_TAGS, ADMIN_TAGS, DISPLAYED_ADMIN_TAGS = jsonIO.load(f"{DIR}/special_tags.json")
+SPECIAL_TAGS, DISPLAYED_SPECIAL_TAGS, ADMIN_TAGS, DISPLAYED_ADMIN_TAGS = jsonIO.load(f"{DIR}/special_tags.json").values()
 VALID_NAME_CHARS = set("0123456789abcdefghijklmnopqrstuvwxyz_-")
