73b170f842d3e71f17623fbae16e4df286520a18
driftbluestone <driftbluestone@gmail.com>
2026-06-14 01:56:54 -0700

    never changing anything ever again

--- strong_tag_data.py
+++ strong_tag_data.py
@@ -3,4 +3,3 @@ from pathlib import Path
 DIR = Path(__file__).resolve().parent
 SPECIAL_TAGS, DISPLAYED_SPECIAL_TAGS, ADMIN_TAGS, DISPLAYED_ADMIN_TAGS = jsonIO.load(f"{DIR}/special_tags.json")
 VALID_NAME_CHARS = set("0123456789abcdefghijklmnopqrstuvwxyz_-")
-__all__ = ["CallableModule", "SPECIAL_TAGS", "DISPLAYED_SPECIAL_TAGS", "ADMIN_TAGS", "DISPLAYED_ADMIN_TAGS", "VALID_NAME_CHARS"]
