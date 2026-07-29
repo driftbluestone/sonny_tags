d2ae798c4c48acf0b804410193ef1c4043dfe483
driftbluestone <driftbluestone@gmail.com>
2026-07-28 21:43:54 -0700

    fix %t quota if user not in db

--- functions/quota.py
+++ functions/quota.py
@@ -6,6 +6,8 @@ from .. import tag_utils
 @CallableModule
 async def tag_quota(ctx: commands.Context, message: list[str]):
     space = db.get("sonny_tags$users", (ctx.author.id,), ("user_id",), ("space",))
+    if space is None:
+        space = 0
     KiB = space / 1024
     percent = 100 * (space / 1048576)
     return await ctx.reply(f":information_source: Using {KiB:.2f}/1024 KiB ({percent:.2f}%)")
