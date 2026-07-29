a22cc9732a8539cd2c271223da86c784c4c3fc8d
driftbluestone <driftbluestone@gmail.com>
2026-07-28 18:24:17 -0700

    add %t quota

---
+++ functions/quota.py
@@ -0,0 +1,11 @@
+from discord.ext import commands
+from db import db
+from ..strong_tag_data import *
+from .. import tag_utils
+
+@CallableModule
+async def tag_quota(ctx: commands.Context, message: list[str]):
+    space = db.get("sonny_tags$users", (ctx.author.id,), ("user_id",), ("space",))
+    KiB = space / 1024
+    percent = 100 * (space / 1048576)
+    return await ctx.reply(f":information_source: Using {KiB:.2f}/1024 KiB ({percent:.2f}%)")
