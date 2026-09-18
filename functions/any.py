9ab7eb1a3f672cd046b44832cb3e9e6e6560fd30
driftbluestone <driftbluestone@gmail.com>
2026-09-17 21:13:01 -0700

    dn

--- functions/any.py
+++ functions/any.py
@@ -4,8 +4,7 @@ from psycopg import sql
 from .. import tags
 from ..strong_tag_data import *
 
-
 @CallableModule
 async def tag_any(ctx: commands.Context, message: list):
-    q = sql.SQL("SELECT * FROM sonny_tags$tags ORDER BY RANDOM() LIMIT 1;")
+    q = sql.SQL(f"SELECT * FROM {db.SCHEMA.as_string()}.sonny_tags$tags ORDER BY RANDOM() LIMIT 1;")
     await tags.get_tag(ctx, db.single(q)[0], [""])
